5c25507fb60a400f2a1f9c05d53c56db889652fb
Kim Kunniah <kunniahkimberlee@gmail.com>
2022-09-02 06:52:13 -0700

    Task 1

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    """
+    replaces all occurrences of an element by another in a new list
+    """
+    return ([elem if elem != search else replace for elem in my_list])

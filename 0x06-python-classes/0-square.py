50b96804f3d9613a74ff0165fb8e3c4791e80a67
Kim Kunniah <kunniahkimberlee@gmail.com>
2022-09-14 06:50:27 -0700

    Task 0

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 14 15:48 2022
+@author: Kim Kunniah
+"""
+
+class Square:
+    """Empty class Square that defines a square
+
+    Attributes:
+        empty
+    """
+
+    pass

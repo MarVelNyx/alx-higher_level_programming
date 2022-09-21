f3aae028b98bfa2a596bd967cb599203505d539a
Kim Kunniah <kunniahkimberlee@gmail.com>
2022-09-21 10:05:25 -0700

    Task 0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 21 2022
+@author: Kim Kunniah
+"""
+
+
+class Rectangle:
+    """Empty class Rectangle defines retangle figure
+
+    Attributes:
+        empty
+    """
+    pass

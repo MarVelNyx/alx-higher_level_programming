78646a906b142c619b9a9561419c08cda34d50ca
Kim Kunniah <kunniahkimberlee@gmail.com>
2022-10-12 00:10:33 -0700

    Task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 12 09:05 2022
+
+@author: Kim Kunniah
+"""
+
+
+def lookup(obj):
+    """
+    Returns all objects in an objects dictionary as a list
+    """
+    return dir(obj)

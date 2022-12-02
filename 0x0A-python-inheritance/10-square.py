7ce71738e3014ace0bf81c2c457ea2014fd7ad63
Kim Kunniah <kunniahkimberlee@gmail.com>
2022-12-01 23:21:34 -0800

    Try fix bug

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -24,3 +24,12 @@ class Square(Rectangle):
         super().__init__(size, size)
         self.integer_validator("size", size)
         self.__size = size
+
+    def area(self):
+        """
+        Calculates area of the square
+
+        Return:
+            the area of the square
+        """
+        return self.__size

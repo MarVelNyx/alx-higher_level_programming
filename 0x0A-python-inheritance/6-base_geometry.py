193cec2f1c214ee240285eb1ca3629190d60e5f3
Kim Kunniah <kunniahkimberlee@gmail.com>
2022-12-01 22:17:46 -0800

    Whitespace removed

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -6,13 +6,12 @@ Created on Wed Oct 12 12:19 2022
 @author: Kim Kunniah
 """
 
-
 class BaseGeometry():
     """
     An empty class
     """
     pass
-    
+
     def area(self):
         """
         Public instance method that calculates the area

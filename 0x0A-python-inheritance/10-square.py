a9256c0520456f0a4fd74875334901133fb2b811
Kim Kunniah <kunniahkimberlee@gmail.com>
2022-12-01 23:13:18 -0800

    Task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# -*- coding: uft-8 -*-
+"""
+Created on Fri Dec 02 09:08 2022
+
+@author: Kim Kunniah
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    A Square class ahpe, inherits from Rectangle and BaseGeometry
+    """
+    def __init__(self, size):
+        """
+        Init function for Square
+
+        Attributes:
+            size (int): The size of the square
+        """
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+        self.__size = size

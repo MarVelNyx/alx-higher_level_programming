b99537ac9280cadc1db3c126b6305cabe957f6f6
Kim Kunniah <kunniahkimberlee@gmail.com>
2022-12-10 00:30:15 -0800

    Create Readme and directories

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat 10 Dec 2022 10:21
+
+@author: Kim
+"""
+import json
+
+
+class Base:
+    """
+    Class Base
+
+    Attributes:
+        nb_objects (int): Private class attribute
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Class constructor
+
+        Attributes:
+            id (int): An integer input for id
+        """
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
